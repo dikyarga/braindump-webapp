97ec671927aa7035fcb6b0bc20b3eb1ae771a2a7
dikyarga <dikyarga.id@gmail.com>
2021-05-03 00:09:45 +0700

    fix e2e test

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -3,16 +3,12 @@ describe("Homepage", () => {
         cy.visit("/");
     });
     it("Brings header", () => {
-        cy.getBySel("main-heading").should("contain.text", "superplate");
+        cy.getBySel("main-heading").should("contain.text", "BrainDump App");
     });
 
-    it("Should have true href", () => {
-        // https://github.com/cypress-io/cypress-example-recipes/blob/master/examples/testing-dom__tab-handling-links/cypress/integration/tab_handling_anchor_links_spec.js
-        cy.getBySel("docs-btn-anchor")
-            .should("have.prop", "href")
-    });
+    it("Should be able to create new idea", () => {
+        const writeButton = cy.get("button");
 
-    it("Should have icons", () => {
-        cy.getBySel("icon").should("have.length", 6);
+        writeButton.contains("Write idea");
     });
 });
